0542997fe77ec8082a4d71dc8e1475960691c823
p-nanaware <pnanaware.umassd.edu>
2024-07-07 16:24:07 -0400

    Initial Commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://master-owl-55.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
